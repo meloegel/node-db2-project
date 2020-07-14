fbf0bf19b6a8a963dcac83bbd0474003a8d67354
meloegel <meloegel@gmail.com>
2020-07-14 16:20:23 -0400

    stretch

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -27,6 +27,22 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.put('/:id', (req, res) => {
+    const changes = req.body
+    const { id } = req.params
+    db('car-dealer').where({ id }).update(changes)
+        .then(ids => {
+            db('car-dealer').where({ id: ids })
+                .then(car => {
+                    res.status(201).json(car)
+                })
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: 'error' })
+        })
+})
+
 router.post('/', (req, res) => {
     const carData = req.body
     db('car-dealer').insert(carData)
@@ -35,6 +51,22 @@ router.post('/', (req, res) => {
                 .then(newCar => {
                     res.status(201).json(newCar)
                 })
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: 'error' })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    db('car-dealer').where({ id }).del()
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({ error: 'not found' })
+            }
         })
         .catch(error => {
             console.log(error)
